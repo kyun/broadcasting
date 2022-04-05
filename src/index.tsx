92baf67779f4d8d74af6666ae4bcced25a706c53
kyun <wls5563@gmail.com>
2022-04-05 20:57:06 +0900

    readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import IndexPage from './pages/IndexPage';
-import {
-  RecoilRoot,
-} from 'recoil';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import IndexPage from "./pages/IndexPage";
+import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,7 +11,7 @@ ReactDOM.render(
       <IndexPage />
     </RecoilRoot>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
